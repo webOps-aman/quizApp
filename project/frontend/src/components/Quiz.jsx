aa5957750e8c7d0b0f733da85da7b2f6731fdf2a
webOps-aman <amanhxe@gmail.com>
2025-02-03 22:01:20 +0530

    start new project quiz app

---
+++ project/frontend/src/components/Quiz.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Quiz.css';
+
+const Quiz = () => {
+  return (
+    <>
+        <div className='container'>
+            <h1>Quiz App</h1>
+        </div>
+    </>
+  )
+}
+
+export default Quiz
